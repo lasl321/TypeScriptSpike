fb293f1c296014764f0abef44ec9dac1656fe729
Luis Sotomayor <LSotomayor@futuredontics.com>
2015-04-24 20:17:07 -0700

    Added scripts and bundling.

---
+++ TypeScriptSpike/App_Start/BundleConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Optimization;
+
+namespace TypeScriptSpike
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/angular.js"));
+        }
+    }
+}
