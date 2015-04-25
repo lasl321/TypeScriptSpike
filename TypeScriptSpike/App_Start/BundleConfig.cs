23f9ee04ac7a571560ed041bba617a1d1f6de016
Luis Sotomayor <LSotomayor@futuredontics.com>
2015-04-24 20:26:15 -0700

    Added bootstrap.

--- TypeScriptSpike/App_Start/BundleConfig.cs
+++ TypeScriptSpike/App_Start/BundleConfig.cs
@@ -6,7 +6,12 @@ namespace TypeScriptSpike
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
             bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/angular.js"));
+            bundles.Add(new ScriptBundle("~/bundles/application").IncludeDirectory("~/Scripts/Application/", "*.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/bootstrap").Include("~/Content/bootstrap.css"));
         }
     }
 }
